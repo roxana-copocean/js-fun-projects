f7e8ae7c4b2b885abca1cb4f1e7887cf883e59d3
Roxana Copocean <roxana.mica@gmail.com>
2023-02-10 17:09:33 +0200

    end

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 window.addEventListener('keydown', (e) => {
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 	console.log(audio);
 	if (!audio) return;
 	audio.currentTime = 0;
 	audio.play();
+	key.classList.add('playing');
 });
+
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) =>
+	key.addEventListener('transitionend', (e) => {
+		if (e.propertyName !== 'transform') return;
+		e.target.classList.remove('playing');
+	})
+);
